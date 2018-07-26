54dffa5685d590082bb9e9e9798d16fcd5550645
Seikilos <seikilos@git>
2018-07-26 18:02:41 +0200

    Refactored console logger to utilize the common output logger.

--- Host/Implementation/ConsoleLogger.cs
+++ Host/Implementation/ConsoleLogger.cs
@@ -7,67 +7,13 @@ using Contracts;
 
 namespace VersatileFileComparerHost.Implementation
 {
-    class ConsoleLogger : ILogger
+    /// <summary>
+    /// Simple console logger relying on common output logger
+    /// </summary>
+    class ConsoleLogger : CommonOutputLogger
     {
-        public bool HadErrors { get; private set; } = false;
-
-        private enum LogType
+        public ConsoleLogger( ) : base(Console.WriteLine, Console.Error.WriteLine)
         {
-            INFO,
-            WARNING,
-            ERROR,
-            EXCEPTION
-        }
-
-        public void Info( string msg, params object[] parameters )
-        {
-           _log(LogType.INFO, msg, parameters);
-        }
-
-        public void Warning( string msg, params object[] parameters )
-        {
-            _log(LogType.WARNING, msg, parameters);
-        }
-
-        public void Error( string msg, params object[] parameters )
-        {
-            _log(LogType.ERROR, msg, parameters);
-        }
-
-        public void Exception( string msg, Exception innerException )
-        {
-            var sb = new StringBuilder();
-           sb.AppendLine(msg);
-
-            var e = innerException;
-            while ( e != null )
-            {
-                sb.AppendLine("with inner exception:");
-                sb.AppendLine(e.ToString());
-                e = innerException.InnerException;
-            }
-
-
-            _log(LogType.EXCEPTION, msg);
-        }
-
-        private void _log(LogType type, string msg, params object[] parameters)
-        {
-            switch ( type )
-            {
-                case LogType.INFO:
-                case LogType.WARNING:
-                    Console.WriteLine(msg, parameters);
-                    break;
-
-                case LogType.ERROR:
-                case LogType.EXCEPTION:
-                    HadErrors = true;
-                    Console.Error.WriteLine(msg, parameters);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(type), type, null);
-            }
         }
     }
 }
